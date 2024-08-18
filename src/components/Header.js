8f266b3439b088245da194bf29408597a34215e0
Addhyan01 <addhyankumar@gmail.com>
2024-08-19 00:57:41 +0530

    Navigation is Working Proper

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Nav } from 'react-bootstrap'
+import Navbar from './Navbar'
+
 
 export default function Header() {
   return (
-    <div>Header</div>
+
+   <Navbar />
+ 
+
+
   )
 }
